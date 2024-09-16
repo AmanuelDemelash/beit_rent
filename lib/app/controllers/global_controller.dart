119a7e37072b3528014f9a43dbf449c13b543ac2
AmanuelDemelash <amanueldemelash12@gmail.com>
2024-09-16 12:41:07 +0300

    some changes

--- lib/app/controllers/global_controller.dart
+++ lib/app/controllers/global_controller.dart
@@ -1,13 +1,36 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class GlobalController extends GetxController {
-
-  RxBool isLogIn=false.obs;
-  RxMap<String,dynamic> user=RxMap<String,dynamic>();
+  late StreamSubscription subscription;
+  RxBool isLogIn = false.obs;
+  RxMap<String, dynamic> user = RxMap<String, dynamic>();
 
   @override
   void onInit() {
+    subscription = Connectivity().onConnectivityChanged.listen((result) {
+      // Received changes in available connectivity types!
+      for (var element in result) {
+        if (element == ConnectivityResult.none) {
+          Get.rawSnackbar(
+              title: "connection lost",
+              message: "please cheek your internet connection",
+              backgroundColor: Colors.redAccent,
+              icon: const Icon(Icons.signal_wifi_connected_no_internet_4),
+              isDismissible: false,
+              barBlur: 0.9,
+              duration: const Duration(days: 1));
+        } else {
+          if (Get.isSnackbarOpen) {
+            Get.closeCurrentSnackbar();
+          }
+        }
+      }
+    });
     getToken();
     super.onInit();
   }
@@ -15,11 +38,8 @@ class GlobalController extends GetxController {
   Future<void> getToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token') ?? "";
-    String? id = prefs.getString('id') ?? "";
     if (token.isNotEmpty) {
       isLogIn.value = true;
     }
   }
-
-
 }
